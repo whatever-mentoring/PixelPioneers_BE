727bf75a0359e8cee5f73eb8bb9df8c6a4a50758
ChaeAg <dydkglel123@gmail.com>
2023-08-30 23:39:11 +0900

    JDK, 스프링 버전 수정

---
+++ src/main/java/com/example/Posewithme/PosewithmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Posewithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosewithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosewithmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Posewithme/PosewithmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Posewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosewithmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
