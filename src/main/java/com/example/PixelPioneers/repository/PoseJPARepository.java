d365f313dab820add83227be7b18d864996f9b06
cosmos_1885 <dmsrkql104321@gmail.com>
2023-09-10 22:50:46 +0900

    feat: create Pose

---
+++ src/main/java/com/example/PixelPioneers/repository/PoseJPARepository.java
@@ -0,0 +1,11 @@
+package com.example.PixelPioneers.repository;
+
+import com.example.PixelPioneers.entity.Pose;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PoseJPARepository extends JpaRepository<Pose, Integer> {
+    List<Pose> findAllByPeopleCount(int peopleCount);
+}
+
