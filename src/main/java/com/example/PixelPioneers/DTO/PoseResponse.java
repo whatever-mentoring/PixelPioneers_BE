4c761d1dc85bb938329756feac7086de2c6cce74
ChaeAg <dydkglel123@gmail.com>
2023-09-12 18:51:06 +0900

    Pose DTO에 DB 변경사항 반영

--- src/main/java/com/example/PixelPioneers/DTO/PoseResponse.java
+++ src/main/java/com/example/PixelPioneers/DTO/PoseResponse.java
@@ -21,14 +21,14 @@ public class PoseResponse {
         @Getter
         @Setter
         public class PoseDTO {
-            private int id;
-            private String image;
-            private int peopleCount;
+            private int pose_id;
+            private int photo_id;
+            private String photo_imageURL;
 
             public PoseDTO(Pose pose) {
-                this.id = pose.getId();
-                this.image = pose.getImage();
-                this.peopleCount = pose.getPeopleCount();
+                this.pose_id = pose.getPose_id();
+                this.photo_id = pose.getUserPhoto().getPhoto_id();
+                this.photo_imageURL = pose.getUserPhoto().getPhoto_image();
             }
         }
     }
@@ -36,16 +36,26 @@ public class PoseResponse {
     @Getter
     @Setter
     public static class PoseDetailDTO {
-        private int id;
-        private String image;
-        private String hashtag;
-        private int peopleCount;
+        private int pose_id;
+        private int photo_id;
+        private String photo_imageURL;
+        private int photo_public;
+        private String photo_name;
+        private String photo_created_at;
+        private int photo_people_count;
+        private int album_id;
+        private String album_name;
+
 
         public PoseDetailDTO(Pose pose) {
-            this.id = pose.getId();
-            this.image = pose.getImage();
-            this.hashtag = pose.getHashtag();
-            this.peopleCount = pose.getPeopleCount();
+            this.pose_id = pose.getPose_id();
+            this.photo_public = pose.getUserPhoto().getPhoto_public();
+            this.photo_imageURL = pose.getUserPhoto().getPhoto_image();
+            this.photo_name = pose.getUserPhoto().getPhoto_name();
+            this.photo_created_at = pose.getUserPhoto().getPhoto_created_at();
+            this.photo_people_count = pose.getUserPhoto().getPhoto_people_count();
+            this.album_id = pose.getUserPhoto().getAlbum().getAlbum_id();
+            this.album_name = pose.getUserPhoto().getAlbum().getAlbum_name();
         }
     }
 }
