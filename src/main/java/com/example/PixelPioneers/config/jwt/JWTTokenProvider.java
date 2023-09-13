2ea6858e067406fd5afa168b56d68268f468b2a6
cosmos_1885 <dmsrkql104321@gmail.com>
2023-09-14 00:43:30 +0900

    feat: create JWTToken

---
+++ src/main/java/com/example/PixelPioneers/config/jwt/JWTTokenProvider.java
@@ -0,0 +1,36 @@
+package com.example.PixelPioneers.config.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.PixelPioneers.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTTokenProvider {
+    public static final String SECRET = "SecretKey";
+    public static final Long EXP = 1000L * 60 * 30; // 30분
+    public static final String HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer "; // 스페이스 필요함
+
+    public static String create(User user) {
+        String jwt = JWT.create()
+                .withSubject(user.getEmail())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXP))
+                .withClaim("user_id", user.getUser_id())
+                .withClaim("user_role", user.getUser_role())
+                .sign(Algorithm.HMAC512(SECRET));
+        return TOKEN_PREFIX + jwt;
+    }
+
+    public static DecodedJWT verify(String jwt) throws SignatureVerificationException, TokenExpiredException {
+        jwt = jwt.replace(JWTTokenProvider.TOKEN_PREFIX, "");
+        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(SECRET))
+                .build().verify(jwt);
+        return decodedJWT;
+    }
+}
