8d2bc5e0604276bfd25ac0457d67cd1b4c99a6fb
ChaeAg <dydkglel123@gmail.com>
2023-09-16 04:00:41 +0900

    Add Swagger to UserRestController

--- src/main/java/com/example/PixelPioneers/controller/UserRestController.java
+++ src/main/java/com/example/PixelPioneers/controller/UserRestController.java
@@ -4,6 +4,7 @@ import com.example.PixelPioneers.DTO.UserRequest;
 import com.example.PixelPioneers.Service.UserService;
 import com.example.PixelPioneers.config.jwt.JWTTokenProvider;
 import com.example.PixelPioneers.config.utils.ApiUtils;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -19,6 +20,7 @@ public class UserRestController {
     /**
      * 이메일 중복 확인
      */
+    @ApiOperation(value="이메일 중복 확인", notes = "중복된 이메일이 있는지 확인합니다.")
     @PostMapping("/email/check")
     public ResponseEntity<?> emailCheck(@RequestBody @Valid UserRequest.EmailCheckDTO emailCheckDTO, Errors errors) {
         userService.emailCheck(emailCheckDTO.getEmail());
@@ -28,6 +30,7 @@ public class UserRestController {
     /**
      * 회원가입
      */
+    @ApiOperation(value="회원가입", notes = "전달받은 회원 정보를 바탕으로 회원을 생성합니다.")
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody @Valid UserRequest.JoinDTO requestDTO, Errors errors) {
         userService.join(requestDTO);
@@ -37,12 +40,14 @@ public class UserRestController {
     /**
      * 로그인
      */
+    @ApiOperation(value="로그인", notes = "입력받은 정보의 회원이 있는지 검색하여 로그인합니다.")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid UserRequest.LoginDTO requestDTO, Errors errors) {
         String jwt = userService.login(requestDTO);
         return ResponseEntity.ok().header(JWTTokenProvider.HEADER, jwt).body(ApiUtils.success(true));
     }
 
+    @ApiOperation(value="카카오 로그인", notes = "카카오로 로그인합니다.")
     @GetMapping("/login/kakao")
     public void kakaoLogin(@RequestParam String code) {
         String access_token = userService.getKakaoAccessToken(code);
