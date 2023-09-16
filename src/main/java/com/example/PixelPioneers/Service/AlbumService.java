0b7fc943985b55b431ba202e1bc3f1ab2a91838b
cosmos_1885 <dmsrkql104321@gmail.com>
2023-09-16 15:36:17 +0900

    refactor: update Service

--- src/main/java/com/example/PixelPioneers/Service/AlbumService.java
+++ src/main/java/com/example/PixelPioneers/Service/AlbumService.java
@@ -1,12 +1,21 @@
 package com.example.PixelPioneers.Service;
 
+import com.example.PixelPioneers.DTO.AlbumRequest;
 import com.example.PixelPioneers.DTO.AlbumResponse;
+import com.example.PixelPioneers.DTO.PhotoResponse;
 import com.example.PixelPioneers.DTO.Photo_AlbumResponse;
 
+import com.example.PixelPioneers.config.errors.exception.Exception400;
+import com.example.PixelPioneers.config.errors.exception.Exception404;
 import com.example.PixelPioneers.entity.Album;
 import com.example.PixelPioneers.entity.Photo;
+import com.example.PixelPioneers.entity.User;
+import com.example.PixelPioneers.entity.User_Album;
 import com.example.PixelPioneers.repository.AlbumJPARepository;
 
+import com.example.PixelPioneers.repository.PhotoJPARepository;
+import com.example.PixelPioneers.repository.UserJPARepository;
+import com.example.PixelPioneers.repository.User_AlbumJPARepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,39 +32,63 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class AlbumService {
-    private final AlbumJPARepository albumRepository;
+    private final UserJPARepository userJPARepository;
+    private final User_AlbumJPARepository user_albumJPARepository;
+    private final AlbumJPARepository albumJPARepository;
+    private final PhotoJPARepository photoJPARepository;
 
-    public List<AlbumResponse.FindAllDTO> findAll(int page) {
+//    public AlbumResponse.FindByIdDTO findById(int album_id) {
+//        Optional<Album> album = albumRepository.findById(album_id);
+//        return new AlbumResponse.FindByIdDTO(album);
+//    }
+
+    public void addAlbum(AlbumRequest.AlbumAddDTO requestDTO, User sessionUser) {
+        String name = requestDTO.getName();
+        String image = requestDTO.getImage();
+        Album newAlbum = Album.builder().name(name).image(image).build();
+        Album album = albumJPARepository.save(newAlbum);
+
+        List<Integer> userIdList = requestDTO.getUserIdList();
+        for (Integer userId: userIdList) {
+            User user = userJPARepository.findById(userId)
+                    .orElseThrow(() -> new Exception404("사용자가 존재하지 않습니다."));
+            User_Album newUser_Album = User_Album.builder().user(user).album(album).build();
+            user_albumJPARepository.save(newUser_Album);
+        }
+    }
+
+    public List<AlbumResponse.AlbumListDTO> findAlbumListByUser(User user, int page) {
         Pageable pageable = PageRequest.of(page, 10);
 
-        Page<Album> pageContent = albumRepository.findAll(pageable);
+        Page<Album> pageContent = user_albumJPARepository.findByUserId(user.getId(), pageable);
 
-        List<AlbumResponse.FindAllDTO> responseDTOs = pageContent.getContent().stream()
-                .map(album -> new AlbumResponse.FindAllDTO(album))
+        List<AlbumResponse.AlbumListDTO> responseDTOs = pageContent.getContent().stream()
+                .map(album -> new AlbumResponse.AlbumListDTO(album))
                 .collect(Collectors.toList());
         return responseDTOs;
     }
 
-    public AlbumResponse.FindByIdDTO findById(int album_id) {
-        Optional<Album> album = albumRepository.findById(album_id);
-        return new AlbumResponse.FindByIdDTO(album);
-    }
+    public List<PhotoResponse.PhotoListDTO> findPhotoList(int id, int page) {
+        Pageable pageable = PageRequest.of(page, 10);
 
-    public AlbumResponse.FindByIdDTO create(Album new_album){
-        albumRepository.save(new_album);
-        Optional<Album> album = albumRepository.findById(new_album.getAlbum_id());
-        return new AlbumResponse.FindByIdDTO(album);
-    }
+        Page<Photo> pageContent = photoJPARepository.findByAlbumId(id, pageable);
 
-    public List<Photo_AlbumResponse.FindAllDTO> Photo_FindBy_Fk(int album_id){
-//        List<Photo> photoList = albumRepository.findById(album_id).get().getPhotos();
-        List<Photo_AlbumResponse.FindAllDTO> responseDTOs = new ArrayList<>();
-
-//        for(Photo item : photoList){
-//            responseDTOs.add(new Photo_AlbumResponse.FindAllDTO(item));
-//        }
-
+        List<PhotoResponse.PhotoListDTO> responseDTOs = pageContent.getContent().stream()
+                .map(photo -> new PhotoResponse.PhotoListDTO(photo))
+                .collect(Collectors.toList());
         return responseDTOs;
     }
+
+
+//    public List<Photo_AlbumResponse.FindAllDTO> Photo_FindBy_Fk(int album_id){
+////        List<Photo> photoList = albumRepository.findById(album_id).get().getPhotos();
+//        List<Photo_AlbumResponse.FindAllDTO> responseDTOs = new ArrayList<>();
+//
+////        for(Photo item : photoList){
+////            responseDTOs.add(new Photo_AlbumResponse.FindAllDTO(item));
+////        }
+//
+//        return responseDTOs;
+//    }
 
 }
--- src/main/java/com/example/PixelPioneers/Service/PhotoService.java
+++ src/main/java/com/example/PixelPioneers/Service/PhotoService.java
@@ -1,7 +1,14 @@
 package com.example.PixelPioneers.Service;
 
+import com.example.PixelPioneers.DTO.AlbumRequest;
+import com.example.PixelPioneers.DTO.AlbumResponse;
+import com.example.PixelPioneers.DTO.PhotoRequest;
 import com.example.PixelPioneers.DTO.PhotoResponse;
+import com.example.PixelPioneers.config.errors.exception.Exception404;
+import com.example.PixelPioneers.entity.Album;
 import com.example.PixelPioneers.entity.Photo;
+import com.example.PixelPioneers.entity.User;
+import com.example.PixelPioneers.entity.User_Album;
 import com.example.PixelPioneers.repository.AlbumJPARepository;
 import com.example.PixelPioneers.repository.PhotoJPARepository;
 import lombok.RequiredArgsConstructor;
@@ -19,24 +26,48 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class PhotoService {
-    private final PhotoJPARepository photoRepository;
+    private final PhotoJPARepository photoJPARepository;
     private final AlbumJPARepository albumJPARepository;
 
-    public List<PhotoResponse.FindAllDTO> findAll(int page) {
-        Pageable pageable = PageRequest.of(page, 15);
+    public void addPhoto(int id, PhotoRequest.PhotoAddDTO requestDTO) {
+        Album album = albumJPARepository.findById(id)
+                .orElseThrow(() -> new Exception404("사진첩을 찾을 수 없습니다."));
 
-        Page<Photo> pageContent = photoRepository.findAll(pageable);
-
-        List<PhotoResponse.FindAllDTO> responseDTOs = pageContent.getContent().stream()
-                .map(photo -> new PhotoResponse.FindAllDTO(photo))
-                .collect(Collectors.toList());
-        return responseDTOs;
+        Photo newPhoto = Photo.builder().name(requestDTO.getName()).image(requestDTO.getImage()).peopleCount(requestDTO.getPeopleCount()).created_at(requestDTO.getCreated_at()).open(requestDTO.isOpen()).build();
+        photoJPARepository.save(newPhoto);
     }
 
-    public PhotoResponse.FindByIdDTO findById(int photo_id) {
-        Optional<Photo> photo = photoRepository.findById(photo_id);
-        return new PhotoResponse.FindByIdDTO(photo);
-    }
+//    public void addPhoto(AlbumRequest.AlbumAddDTO requestDTO, User sessionUser) {
+//        String name = requestDTO.getName();
+//        String image = requestDTO.getImage();
+//        Album newAlbum = Album.builder().name(name).image(image).build();
+//        Album album = albumJPARepository.save(newAlbum);
+//
+//        List<Integer> userIdList = requestDTO.getUserIdList();
+//        for (Integer userId: userIdList) {
+//            User user = userJPARepository.findById(userId)
+//                    .orElseThrow(() -> new Exception404("사용자가 존재하지 않습니다."));
+//            User_Album newUser_Album = User_Album.builder().user(user).album(album).build();
+//            user_albumJPARepository.save(newUser_Album);
+//        }
+//    }
+
+
+//    public List<PhotoResponse.FindAllDTO> findAll(int page) {
+//        Pageable pageable = PageRequest.of(page, 15);
+//
+//        Page<Photo> pageContent = photoRepository.findAll(pageable);
+//
+//        List<PhotoResponse.FindAllDTO> responseDTOs = pageContent.getContent().stream()
+//                .map(photo -> new PhotoResponse.FindAllDTO(photo))
+//                .collect(Collectors.toList());
+//        return responseDTOs;
+//    }
+//
+//    public PhotoResponse.FindByIdDTO findById(int photo_id) {
+//        Optional<Photo> photo = photoRepository.findById(photo_id);
+//        return new PhotoResponse.FindByIdDTO(photo);
+//    }
 
     //public PhotoResponse.FindByIdDTO create(Photo new_photo){
     //    photoRepository.save(new_photo);
@@ -44,16 +75,16 @@ public class PhotoService {
     //    return new PhotoResponse.FindByIdDTO(photo);
     //}
 
-    public PhotoResponse.FindByIdDTO create_new(Photo new_photo, int album_id){
-        new_photo = new_photo.toBuilder()
-                .album(albumJPARepository.findById(album_id).get())
-                .build();
-
-        photoRepository.save(new_photo);
-
-        Optional<Photo> photo = photoRepository.findById(new_photo.getPhoto_id());
-        return new PhotoResponse.FindByIdDTO(photo);
-    }
+//    public PhotoResponse.FindByIdDTO create_new(Photo new_photo, int album_id){
+//        new_photo = new_photo.toBuilder()
+//                .album(albumJPARepository.findById(album_id).get())
+//                .build();
+//
+//        photoRepository.save(new_photo);
+//
+//        Optional<Photo> photo = photoRepository.findById(new_photo.getPhoto_id());
+//        return new PhotoResponse.FindByIdDTO(photo);
+//    }
 
 
 }
