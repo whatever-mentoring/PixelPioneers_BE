c2eecf11a9fb0df0de3c7e6f714fc00e5aea3229
cosmos_1885 <dmsrkql104321@gmail.com>
2023-09-18 02:53:06 +0900

    refactor: update Controller

--- src/main/java/com/example/PixelPioneers/controller/PoseRestController.java
+++ src/main/java/com/example/PixelPioneers/controller/PoseRestController.java
@@ -10,26 +10,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class PoseRestController {
-    private final PoseService poseService;
 
-    @GetMapping("/poses")
-    public ResponseEntity<?> poseListByPeopleCount(@RequestParam(value = "peopleCount", defaultValue = "0") int peopleCount) {
-        PoseResponse.PoseListDTO responseDTOs = poseService.poseListByPeopleCount(peopleCount);
-        return ResponseEntity.ok(ApiUtils.success(responseDTOs));
-    }
+    private final PoseService poseService;
 
     @GetMapping("/poses/random")
     public ResponseEntity<?> randomPoseDetailByPeopleCount(@RequestParam(value = "peopleCount") int peopleCount) {
-        PoseResponse.PoseDetailDTO responseDTO = poseService.randomPoseDetailByPeopleCount(peopleCount);
+        PoseResponse.PoseDTO responseDTO = poseService.randomPoseDetailByPeopleCount(peopleCount);
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
-    @GetMapping("/poses/{id}")
-    public ResponseEntity<?> poseDetail(@PathVariable int id) {
-        PoseResponse.PoseDetailDTO responseDTO = poseService.poseDetail(id);
-        return ResponseEntity.ok(ApiUtils.success(responseDTO));
+    @GetMapping("/poses")
+    public ResponseEntity<?> adminPoseListByPeopleCount(@RequestParam(value = "category") String category, @RequestParam(value = "peopleCount", defaultValue = "0") int peopleCount) {
+        List<PoseResponse.PoseDTO> responseDTOs = poseService.poseListByCategoryAndPeopleCount(category, peopleCount);
+        return ResponseEntity.ok(ApiUtils.success(responseDTOs));
     }
 }
