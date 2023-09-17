32d17c7c2727b3d35f6dc9dd6e2cc3638d347047
cosmos_1885 <dmsrkql104321@gmail.com>
2023-09-18 02:54:23 +0900

    refactor: update Repository

--- src/main/java/com/example/PixelPioneers/repository/PoseJPARepository.java
+++ src/main/java/com/example/PixelPioneers/repository/PoseJPARepository.java
@@ -1,11 +1,21 @@
 package com.example.PixelPioneers.repository;
 
+import com.example.PixelPioneers.entity.Album;
+import com.example.PixelPioneers.entity.Photo;
 import com.example.PixelPioneers.entity.Pose;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface PoseJPARepository extends JpaRepository<Pose, Integer> {
-    List<Pose> findAllByPeopleCount(int peopleCount);
+//    @Query("select p from Pose p where p.photo.album.id in :albumIdList")
+//    List<Pose> findByAlbumId(Iterable<Integer> albumIdList);
+
+    @Query("select p from Pose p where p.photo.album.id in :albumIdList and p.photo.peopleCount = :peopleCount")
+    List<Pose> findByAlbumIdAndPeopleCount(@Param("albumIdList") List<Integer> albumIdList, @Param("peopleCount") int peopleCount);
 }
 
