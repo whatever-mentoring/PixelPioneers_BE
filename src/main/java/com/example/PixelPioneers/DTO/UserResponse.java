764b0237a4cd328b1e4c95e473bdcd7b4fbf46aa
cosmos_1885 <dmsrkql104321@gmail.com>
2023-09-19 23:33:06 +0900

    refactor: update DTO

--- src/main/java/com/example/PixelPioneers/DTO/UserResponse.java
+++ src/main/java/com/example/PixelPioneers/DTO/UserResponse.java
@@ -8,6 +8,34 @@ import lombok.Setter;
 public class UserResponse {
     @Getter
     @Setter
+    public static class LoginDTO {
+        private UserDetailDTO userDetailDTO;
+        private String JWTToken;
+
+        public LoginDTO(User user, String JWTToken) {
+            this.userDetailDTO = new UserDetailDTO(user);
+            this.JWTToken = JWTToken;
+        }
+
+        @Getter
+        @Setter
+        public class UserDetailDTO {
+            private int id;
+            private String email;
+            private String nickname;
+            private String image;
+
+            public UserDetailDTO(User user) {
+                this.id = user.getId();
+                this.email = user.getEmail();
+                this.nickname = user.getNickname();
+                this.image = user.getImage();
+            }
+        }
+    }
+
+    @Getter
+    @Setter
     public static class UserListDTO {
         private int id;
         private String nickname;
