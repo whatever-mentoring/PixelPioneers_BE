e618b304c12bbb62fcced158ec80a1cde4135737
cosmos_1885 <dmsrkql104321@gmail.com>
2023-09-22 22:17:58 +0900

    refactor: update Entity

--- src/main/java/com/example/PixelPioneers/entity/User.java
+++ src/main/java/com/example/PixelPioneers/entity/User.java
@@ -44,8 +44,12 @@ public class User {
         this.role = role;
     }
 
-    public void update(String nickname, String image) {
+    public void updateUserProfile(String nickname, String image) {
         this.nickname = nickname;
         this.image = image;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
     }
 }
