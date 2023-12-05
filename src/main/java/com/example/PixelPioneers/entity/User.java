6ec56fac9ced6a1f580c9a5e06ba424a2aff44d1
cosmos_1885 <dmsrkql104321@gmail.com>
2023-12-05 17:05:44 +0900

    feat: user entity 수정

--- src/main/java/com/example/PixelPioneers/entity/User.java
+++ src/main/java/com/example/PixelPioneers/entity/User.java
@@ -36,6 +36,7 @@ public class User {
 
     @Builder
     public User(String email, String password, String nickname, String image, String role) {
+        this.id = id;
         this.email = email;
         this.password = password;
         this.nickname = nickname;
