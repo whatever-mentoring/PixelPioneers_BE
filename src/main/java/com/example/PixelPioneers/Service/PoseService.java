442401ceb0014f5192c05602f86a0a46c628517e
cosmos_1885 <dmsrkql104321@gmail.com>
2023-12-09 22:56:56 +0900

    feat: ㅔpose 기능 수정

--- src/main/java/com/example/PixelPioneers/Service/PoseService.java
+++ src/main/java/com/example/PixelPioneers/Service/PoseService.java
@@ -1,3 +1,88 @@
+//package com.example.PixelPioneers.Service;
+//
+//import com.example.PixelPioneers.DTO.AlbumResponse;
+//import com.example.PixelPioneers.DTO.PoseResponse;
+//import com.example.PixelPioneers.config.errors.exception.Exception404;
+//import com.example.PixelPioneers.entity.Album;
+//import com.example.PixelPioneers.entity.Pose;
+//import com.example.PixelPioneers.repository.PhotoJPARepository;
+//import com.example.PixelPioneers.repository.PoseJPARepository;
+//import com.example.PixelPioneers.repository.User_AlbumJPARepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.Random;
+//import java.util.stream.Collectors;
+//
+//@Transactional
+//@RequiredArgsConstructor
+//@Service
+//public class PoseService {
+//
+//    private final User_AlbumJPARepository user_albumJPARepository;
+//    private final PoseJPARepository poseJPARepository;
+//
+//    private List<Integer> randomIndexList = new ArrayList<>();
+//
+//    /**
+//     * 랜덤 포즈 조회
+//     * 인원 수에 따른 랜덤 포즈 개별 조회
+//     */
+//    public PoseResponse.PoseDTO randomPoseDetailByPeopleCount(int peopleCount) {
+//        List<Pose> poseList = poseJPARepository.findByPeopleCountAndOpenAndPass(peopleCount, true, "ACCEPT");
+//
+//        if (randomIndexList.size() == 10) {
+//            randomIndexList.clear();
+//        }
+//
+//        Random random = new Random();
+//        random.setSeed(System.currentTimeMillis());
+//
+//        int randomIndex;
+//        while (true) {
+//            randomIndex = random.nextInt(poseList.size());
+//            if (!randomIndexList.contains(randomIndex)) {
+//                randomIndexList.add(randomIndex);
+//                break;
+//            }
+//        }
+//        Pose randomPose = poseList.get(randomIndex);
+//
+//        return new PoseResponse.PoseDTO(randomPose);
+//    }
+//
+//    /**
+//     * 포즈 모아보기
+//     * 카테고리와 인원 수를 선택했을 경우, 카테고리와 인원 수에 따른 포즈 전체 조회
+//     */
+//    public List<PoseResponse.PoseDTO> poseListByCategoryAndPeopleCount(String category, int peopleCount, int page) {
+//        Pageable pageable = PageRequest.of(page, 10);
+//
+//        List<Integer> albumIdList = user_albumJPARepository.findAlbumByRole(category);
+//        Page<Pose> pageContent = poseJPARepository.findByAlbumIdAndPeopleCountAndOpenAndPass(albumIdList, peopleCount, true, "ACCEPT", pageable);
+//
+//        List<PoseResponse.PoseDTO> responseDTOs = pageContent.getContent().stream()
+//                .map(pose -> new PoseResponse.PoseDTO(pose))
+//                .collect(Collectors.toList());
+//
+//        return responseDTOs;
+//    }
+//
+//    public PoseResponse.PoseDTO findPoseDetail(int id) {
+//        Pose pose = poseJPARepository.findById(id)
+//                .orElseThrow(() -> new Exception404("포즈가 존재하지 않습니다."));
+//
+//        return new PoseResponse.PoseDTO(pose);
+//    }
+//}
+
 package com.example.PixelPioneers.Service;
 
 import com.example.PixelPioneers.DTO.AlbumResponse;
@@ -5,7 +90,6 @@ import com.example.PixelPioneers.DTO.PoseResponse;
 import com.example.PixelPioneers.config.errors.exception.Exception404;
 import com.example.PixelPioneers.entity.Album;
 import com.example.PixelPioneers.entity.Pose;
-import com.example.PixelPioneers.repository.PhotoJPARepository;
 import com.example.PixelPioneers.repository.PoseJPARepository;
 import com.example.PixelPioneers.repository.User_AlbumJPARepository;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +100,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -29,30 +112,18 @@ public class PoseService {
     private final User_AlbumJPARepository user_albumJPARepository;
     private final PoseJPARepository poseJPARepository;
 
-    private List<Integer> randomIndexList = new ArrayList<>();
-
     /**
      * 랜덤 포즈 조회
      * 인원 수에 따른 랜덤 포즈 개별 조회
      */
     public PoseResponse.PoseDTO randomPoseDetailByPeopleCount(int peopleCount) {
-        List<Pose> poseList = poseJPARepository.findByPeopleCountAndOpenAndPass(peopleCount, true, "ACCEPT");
-
-        if (randomIndexList.size() == 10) {
-            randomIndexList.clear();
-        }
+        List<Integer> albumIdList = user_albumJPARepository.findAlbumByRole("ROLE_ADMIN");
+        List<Pose> poseList = poseJPARepository.findByAlbumIdAndPeopleCount(albumIdList, peopleCount);
 
         Random random = new Random();
         random.setSeed(System.currentTimeMillis());
 
-        int randomIndex;
-        while (true) {
-            randomIndex = random.nextInt(poseList.size());
-            if (!randomIndexList.contains(randomIndex)) {
-                randomIndexList.add(randomIndex);
-                break;
-            }
-        }
+        int randomIndex = random.nextInt(poseList.size());
         Pose randomPose = poseList.get(randomIndex);
 
         return new PoseResponse.PoseDTO(randomPose);
@@ -66,9 +137,17 @@ public class PoseService {
         Pageable pageable = PageRequest.of(page, 10);
 
         List<Integer> albumIdList = user_albumJPARepository.findAlbumByRole(category);
-        Page<Pose> pageContent = poseJPARepository.findByAlbumIdAndPeopleCountAndOpenAndPass(albumIdList, peopleCount, true, "ACCEPT", pageable);
+        Page<Pose> pageContent = poseJPARepository.findByAlbumIdAndPeopleCount(albumIdList, peopleCount, pageable);
+        List<Pose> poseList = pageContent.toList();
 
-        List<PoseResponse.PoseDTO> responseDTOs = pageContent.getContent().stream()
+        List<Pose> openPoseList = new ArrayList<>();
+        for (Pose pose : poseList) {
+            if (pose.getPhoto().isOpen()) {
+                openPoseList.add(pose);
+            }
+        }
+
+        List<PoseResponse.PoseDTO> responseDTOs = openPoseList.stream()
                 .map(pose -> new PoseResponse.PoseDTO(pose))
                 .collect(Collectors.toList());
 
@@ -81,4 +160,5 @@ public class PoseService {
 
         return new PoseResponse.PoseDTO(pose);
     }
+
 }
