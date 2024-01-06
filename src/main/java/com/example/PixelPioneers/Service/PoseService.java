3360e4c46d0e2566feb809b7337db5793a1beeb7
Yun-YeoJun <yyj8771@naver.com>
2024-01-06 17:11:54 +0900

    랜덤 포즈 추천 로직 수정 & 성능 개선

--- src/main/java/com/example/PixelPioneers/Service/PoseService.java
+++ src/main/java/com/example/PixelPioneers/Service/PoseService.java
@@ -1,11 +1,8 @@
 package com.example.PixelPioneers.Service;
 
-import com.example.PixelPioneers.DTO.AlbumResponse;
 import com.example.PixelPioneers.DTO.PoseResponse;
 import com.example.PixelPioneers.config.errors.exception.Exception404;
-import com.example.PixelPioneers.entity.Album;
 import com.example.PixelPioneers.entity.Pose;
-import com.example.PixelPioneers.repository.PhotoJPARepository;
 import com.example.PixelPioneers.repository.PoseJPARepository;
 import com.example.PixelPioneers.repository.User_AlbumJPARepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -26,29 +24,47 @@ public class PoseService {
     private final User_AlbumJPARepository user_albumJPARepository;
     private final PoseJPARepository poseJPARepository;
 
-    private int index = 0;
+    private final int MAX_PEOPLE_COUNT = 5;
+    private final int DUPLICATION_LIMIT = 10;
 
+
+    private int[] indexes = new int[MAX_PEOPLE_COUNT];
+    private List<Integer>[] randomIndexLists = new List[10];
+    private List<Pose>[] poseLists = new List[MAX_PEOPLE_COUNT];
+
+    @PostConstruct
+    public void postConstructor() {
+        for (int i = 0; i < MAX_PEOPLE_COUNT; i++) {
+            int peopleCount = i + 1;
+            initHelper(peopleCount);
+        }
+    }
+
+    private void initHelper(int peopleCount) {
+        indexes[peopleCount - 1] = 0;
+        poseLists[peopleCount - 1] = poseJPARepository.findByPeopleCountAndOpenAndPass(peopleCount, true, "ACCEPT");
+        randomIndexLists[peopleCount - 1] = makeRandomIndexList(DUPLICATION_LIMIT, poseLists[peopleCount - 1].size());
+    }
 
     /**
      * 랜덤 포즈 조회
      * 인원 수에 따른 랜덤 포즈 개별 조회
+     * 10번의 요청 동안 중복되는 포즈 x
      */
     public PoseResponse.PoseDTO randomPoseDetailByPeopleCount(int peopleCount) {
-        List<Pose> poseList = poseJPARepository.findByPeopleCountAndOpenAndPass(peopleCount, true, "ACCEPT");
-        List<Integer> randomIndexList = makeRandomIndexList(10, poseList.size());
+        if (peopleCount > MAX_PEOPLE_COUNT) throw new Exception404("요청한 peopleCount를 갖는 포즈가 없습니다.");
 
-        if (index == 10 - 1) {
-            index = 0;
-            randomIndexList = makeRandomIndexList(10, poseList.size());
+        if (indexes[peopleCount - 1] == DUPLICATION_LIMIT - 1) {
+            initHelper(peopleCount);
         }
 
-        int randomIndex = randomIndexList.get(index++);
-        Pose randomPose = poseList.get(randomIndex);
+        int randomIndex = randomIndexLists[peopleCount - 1].get(indexes[peopleCount - 1]++);
+        Pose randomPose = poseLists[peopleCount - 1].get(randomIndex);
 
         return new PoseResponse.PoseDTO(randomPose);
     }
 
-    private List<Integer> makeRandomIndexList(int size, int max) {
+    private static List<Integer> makeRandomIndexList(int size, int max) {
         Random random = new Random();
         random.setSeed(System.currentTimeMillis());
 
